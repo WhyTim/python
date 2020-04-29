72f83d084d4b1bd76e189c2e51b2f47ce94d94a7
timx <61598485+timx23@users.noreply.github.com>
2020-04-29 20:24:23 +0300

    Create script3.py

---
+++ script3.py
@@ -0,0 +1,15 @@
+ for i in range(1111):
+  my_number = int(input())
+
+  s11to19 = int(str(my_number)[-2:])
+  if 11<=s11to19<=19:
+      print(str(my_number) + ' дней назад')
+      continue
+  last_number = int(str(my_number)[-1])
+
+  if last_number==1:
+      print(str(my_number) + ' день назад')
+  if 2<=last_number<=4:
+      print(str(my_number) + ' дня назад')
+  if 5<=last_number<=9 or last_number==0:
+      print(str(my_number) + ' дней назад')
