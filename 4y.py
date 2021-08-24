0376fea90aed1665803fafe03fc15077a6fc03f6
timx <61598485+timx23@users.noreply.github.com>
2021-08-24 08:37:00 +0300

    Add files via upload

---
+++ 4y.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\Qx-2000\Desktop\8.ui'
+#
+# Created: Fri Dec 27 18:55:38 2019
+#      by: pyside-uic 0.2.15 running on PySide 1.2.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_Lol(object):
+    def setupUi(self, Lol):
+        Lol.setObjectName("Lol")
+        Lol.resize(400, 273)
+        Lol.setStyleSheet("QDialog {\n"
+"background-color: white;\n"
+"}")
+        self.line_2 = QtGui.QLineEdit(Lol)
+        self.line_2.setGeometry(QtCore.QRect(100, 60, 141, 21))
+        self.line_2.setText("")
+        self.line_2.setObjectName("line_2")
+        self.textBrowser = QtGui.QTextBrowser(Lol)
+        self.textBrowser.setGeometry(QtCore.QRect(120, 10, 191, 41))
+        self.textBrowser.setStyleSheet("QTextBrowser\n"
+"{\n"
+"border: none;\n"
+"\n"
+"}")
+        self.textBrowser.setObjectName("textBrowser")
+        self.textBrowser_2 = QtGui.QTextBrowser(Lol)
+        self.textBrowser_2.setGeometry(QtCore.QRect(10, 60, 81, 31))
+        self.textBrowser_2.setStyleSheet("QTextBrowser{\n"
+"border: none;\n"
+"}")
+        self.textBrowser_2.setObjectName("textBrowser_2")
+        self.textBrowser_3 = QtGui.QTextBrowser(Lol)
+        self.textBrowser_3.setGeometry(QtCore.QRect(10, 90, 81, 31))
+        self.textBrowser_3.setStyleSheet("QTextBrowser{\n"
+"border: none;\n"
+"}")
+        self.textBrowser_3.setObjectName("textBrowser_3")
+        self.textBrowser_4 = QtGui.QTextBrowser(Lol)
+        self.textBrowser_4.setGeometry(QtCore.QRect(0, 120, 101, 31))
+        self.textBrowser_4.setStyleSheet("QTextBrowser{\n"
+"border: none;\n"
+"}")
+        self.textBrowser_4.setObjectName("textBrowser_4")
+        self.line_1 = QtGui.QLineEdit(Lol)
+        self.line_1.setGeometry(QtCore.QRect(100, 90, 141, 21))
+        self.line_1.setText("")
+        self.line_1.setObjectName("line_1")
+        self.line_3 = QtGui.QLineEdit(Lol)
+        self.line_3.setGeometry(QtCore.QRect(100, 120, 141, 21))
+        self.line_3.setText("")
+        self.line_3.setObjectName("line_3")
+        self.line_4 = QtGui.QLineEdit(Lol)
+        self.line_4.setGeometry(QtCore.QRect(100, 150, 141, 21))
+        self.line_4.setText("")
+        self.line_4.setObjectName("line_4")
+        self.textBrowser_5 = QtGui.QTextBrowser(Lol)
+        self.textBrowser_5.setGeometry(QtCore.QRect(10, 150, 81, 31))
+        self.textBrowser_5.setStyleSheet("QTextBrowser{\n"
+"border: none;\n"
+"}")
+        self.textBrowser_5.setObjectName("textBrowser_5")
+        self.textBrowser_6 = QtGui.QTextBrowser(Lol)
+        self.textBrowser_6.setGeometry(QtCore.QRect(0, 180, 101, 81))
+        self.textBrowser_6.setStyleSheet("QTextBrowser{\n"
+"border: none;\n"
+"}")
+        self.textBrowser_6.setObjectName("textBrowser_6")
+        self.line_5 = QtGui.QLineEdit(Lol)
+        self.line_5.setGeometry(QtCore.QRect(100, 180, 141, 21))
+        self.line_5.setText("")
+        self.line_5.setObjectName("line_5")
+        self.pushButton = QtGui.QPushButton(Lol)
+        self.pushButton.setGeometry(QtCore.QRect(300, 220, 91, 31))
+        self.pushButton.setStyleSheet("QPushButton{\n"
+"border: none;\n"
+"background-color:silver;\n"
+"}\n"
+"QPushButton:hover{\n"
+"background-color:grey;\n"
+"}")
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Lol)
+        QtCore.QMetaObject.connectSlotsByName(Lol)
+
+    def retranslateUi(self, Lol):
+        Lol.setWindowTitle(QtGui.QApplication.translate("Lol", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
+        Lol.setToolTip(QtGui.QApplication.translate("Lol", "<html><head/><body><p>Test</p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        Lol.setWhatsThis(QtGui.QApplication.translate("Lol", "<html><head/><body><p>Test</p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.textBrowser.setHtml(QtGui.QApplication.translate("Lol", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt; font-weight:600;\">TELEGRAM BOT</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.textBrowser_2.setHtml(QtGui.QApplication.translate("Lol", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Фамилия</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.textBrowser_3.setHtml(QtGui.QApplication.translate("Lol", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Имя</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.textBrowser_4.setHtml(QtGui.QApplication.translate("Lol", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Отчество</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.textBrowser_5.setHtml(QtGui.QApplication.translate("Lol", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Возраст</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.textBrowser_6.setHtml(QtGui.QApplication.translate("Lol", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Telegram</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton.setText(QtGui.QApplication.translate("Lol", "ГОТОВО", None, QtGui.QApplication.UnicodeUTF8))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    Lol = QtGui.QDialog()
+    ui = Ui_Lol()
+    ui.setupUi(Lol)
+    Lol.show()
+    sys.exit(app.exec_())
+
